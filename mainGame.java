8a7b3c2ca01dc30dd22358702a3306e0c7713744
Rtannu <info.raj.mnit@gmail.com>
2018-07-08 18:36:12 +0530

    This is main file to start game

---
+++ mainGame.java
@@ -0,0 +1,11 @@
+
+
+//this is main class to start game
+public class mainGame {
+
+    public static void main(String[] args){
+    snakeCals snake =  new snakeCals("java snake game",500,500);
+    snake.start();
+    }
+   
+}
